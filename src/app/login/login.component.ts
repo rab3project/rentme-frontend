da8cac0754f1df3407b8e756e034385483f605d9
pkcmeen2003 <meen_khatri@yahoo.com>
2019-11-20 23:58:23 -0500

    adding existing work

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,19 +8,10 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router) { }
   
-username: string;
-password: string;
-
-  ngOnInit() {
-  }
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["user"]);
-    }else {
-      alert("Invalid credentials");
+  constructor() { }
+  ngOnInit() {}
+  
     }
-  }
-  }
+  
 
